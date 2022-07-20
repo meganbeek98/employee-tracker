862273f20cfeb0e2e2ddc338b2615703c3b0f839
meganbeek98 <meganbeek98@gmail.com>
2022-07-20 06:33:55 -0400

    server.js psuedo-code

---
+++ server.js
@@ -0,0 +1,29 @@
+const inquirer = require("inquirer");
+const cTable = require("console.table");
+const sql = require('./db/query_lib');
+const cHelper = require('./lib/connection');
+
+// ADD NEW DEPARTMENT
+
+
+// ADD NEW EMPLOYEE
+
+
+// ADD EMPLOYEE ROLE
+
+
+// VIEW ALL DEPARTMENTS
+
+
+// VIEW ALL ROLES
+
+
+// VIEW ALL EMPLOYEES
+
+
+
+// DELETE EMPLOYEE
+// UPDATE EMPLOYEE'S ROLE 
+// UPDATE EMPLOYEE'S MANAGER
+// VIEW ALL DEPARTMENT'S BUDGETS
+// VIEW EMPLOYEES IN A SPECIFIC DEPARTMENT
