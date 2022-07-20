2b6ee8707159acb303c2881643cf392ebc048745
meganbeek98 <meganbeek98@gmail.com>
2022-07-20 07:15:42 -0400

    view all depts, roles, employees

--- server.js
+++ server.js
@@ -4,6 +4,28 @@ const sql = require('./db/query_lib');
 const cHelper = require('./lib/connection');
 
 // ADD NEW DEPARTMENT
+const newDept = async () => {  
+
+    const deptartment = await inquirer.prompt([
+       {
+         type: "input",
+         name: "name",
+         message: "What is the name of the Department",
+         validate: (name) =>{
+           if (name) {
+             return true;
+           } else {
+             console.log(" Please Enter a Department Name!")
+             return false;
+           }
+         },
+      },
+    ]);
+  
+    await sql.addDept(deptartment);
+  
+    chooseRequest();
+}
 
 
 // ADD NEW EMPLOYEE
@@ -13,12 +35,46 @@ const cHelper = require('./lib/connection');
 
 
 // VIEW ALL DEPARTMENTS
+const viewDepts = () => {
+    sql.getDepts()
 
+    .then(([rows]) => {
+        console.log('\n');
+        console.log(cTable.getTable(rows));
+    })
 
-// VIEW ALL ROLES
+    .then(()=> {
+        chooseRequest();
+    }) 
+}
 
+// View All Roles
+    const viewRoles = () => {
+    sql.getRoles()
+
+    .then(([rows]) => {
+        console.log('\n');
+        console.log(cTable.getTable(rows));
+    })
+
+    .then(()=> {
+        chooseRequest();
+    })
+}
 
 // VIEW ALL EMPLOYEES
+const viewEmps = () => {
+    sql.getEmps()
+  
+    .then(([rows]) => {
+      console.log('\n');
+      console.log(cTable.getTable(rows));
+    })
+  
+    .then(()=> {
+        chooseRequest();
+    }) 
+}
 
 
 
